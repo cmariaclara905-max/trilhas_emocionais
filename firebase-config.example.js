c3cead09461307b7e069b44feaa87020d55e3668
cmariaclara905-max <cmariaclara905@gmail.com>
2026-09-02 21:01:50 -0300

    Publicar firebase-config.example.js

---
+++ firebase-config.example.js
@@ -0,0 +1,11 @@
+// Copie este arquivo como firebase-config.js e preencha com as credenciais
+// públicas do projeto Firebase criado para a escola. Não inclua chaves privadas.
+export const firebaseConfig = {
+  apiKey: 'SUA_API_KEY',
+  authDomain: 'SEU_PROJETO.firebaseapp.com',
+  projectId: 'SEU_PROJETO',
+  appId: 'SEU_APP_ID'
+};
+
+export const allowedDomains = ['unis.edu.br', 'alunos.edu.br'];
+
